13fb30cd92a17f9cb67bf77db202999a7f947a07
Auguspo <augustpolet@gmail.com>
2023-02-18 13:20:53 -0300

    Get product by id

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { getProductById } from '../fetcher';
 
 const ProductDetail = () => {
+  const [product, setProduct] = useState({errorMessage: '', data:[]});
+  const {productId} = useParams();
+   
+  useEffect(() => {
+    const fetchData = async () =>{
+      const responseObject = await getProductById(productId)
+      setProduct(responseObject)
+    }
+    fetchData()
+  }, [productId])
+
   return (
-    <div>productDetails</div>
+    <div>ProductDetail title: {product.data.title}</div>
   )
 }
 
